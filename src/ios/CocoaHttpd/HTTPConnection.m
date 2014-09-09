472022751b3ca72bd2afa2c2b7072fedb1def5c1
Slava Kim <slava@meteor.com>
2014-09-08 20:17:48 -0700

    c

--- src/ios/CocoaHttpd/HTTPConnection.m
+++ src/ios/CocoaHttpd/HTTPConnection.m
@@ -1651,10 +1651,10 @@ static NSMutableArray *recentNonces;
 		}
 	}
 
-  NSLog(@"should return fullpath %@ for path %@, we can try %@", fullPath, path, [[NSURL URLWithString:@"../cordova.js" relativeToURL:docRoot] absoluteURL]);
+  NSLog(@"should return fullpath %@ for path %@, we can try %@", fullPath, path, [[NSURL URLWithString:@"../cordova.js" relativeToURL:docRoot] absoluteString]);
   // XXX HACKHACK serve cordova.js from the containing folder
   if ([path isEqualToString:@"/cordova.js"])
-    return [[NSURL URLWithString:@"../cordova.js" relativeToURL:docRoot] absoluteURL];
+    return [[NSURL URLWithString:@"../cordova.js" relativeToURL:docRoot] absoluteString];
 
 	return fullPath;
 }
